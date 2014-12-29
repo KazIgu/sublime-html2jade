3126818412350991bcb2e1dcc44fbfd095822f98
KazIgu <kaz.igu.21@gmail.com>
2014-12-30 02:48:58 +0900

    delete web api

--- html2jade.py
+++ html2jade.py
@@ -56,32 +56,15 @@ class HTHTools:
 	@classmethod
 	def post_html_return_jade(self, html):
 		plugin_settings = sublime.load_settings('html2jade.sublime-settings')
-		use_local = plugin_settings.get('use_local')
-		if use_local:
-			cmd = '''
-				html2jade --bodyless --donotencode<< EOF
-				%s
-			''' % html
-			cwd = None
-			env = {"PATH": plugin_settings.get('binDir')}
+		cmd = '''
+			html2jade --bodyless --donotencode<< EOF
+			%s
+		''' % html
+		cwd = None
+		env = {"PATH": plugin_settings.get('binDir')}
 
-			jade = Popen(cmd, env=env, cwd=cwd, stdout=PIPE, stderr=PIPE, shell=True)
-			jade = jade.communicate(input="".encode("utf8"))
-			jade = jade[0].decode('utf8')
-			return jade
+		jade = Popen(cmd, env=env, cwd=cwd, stdout=PIPE, stderr=PIPE, shell=True)
+		jade = jade.communicate(input="".encode("utf8"))
+		jade = jade[0].decode('utf8')
+		return jade
 
-		else:
-			host = 'http://html2jade.aaron-powell.com/convert'
-			values =  {'html': html}
-			data = urllib.parse.urlencode(values)
-			binary_data = data.encode('utf-8')
-			req = urllib.request.Request(host, binary_data)
-			response_stream = urllib.request.urlopen(req)
-			result = json.loads(response_stream.read().decode('utf-8'))
-
-			if result["jade"]:
-				result["jade"] = result["jade"].replace('html\n','')
-				result["jade"] = result["jade"].replace('body\n','')
-				return result["jade"].strip()
-			else:
-				return None
